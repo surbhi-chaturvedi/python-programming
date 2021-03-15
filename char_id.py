cae729e533f428e39b0700660f545bb367b9599e
surbhi-chaturvedi <80679803+surbhi-chaturvedi@users.noreply.github.com>
2021-03-15 15:24:34 +0530

    Add files via upload

---
+++ char_id.py
@@ -0,0 +1,15 @@
+ch=raw_input()
+if(ch>='A'and ch<='Z'):
+    print(ch,"is an upper case alphabet")
+elif(ch>='a'and ch<='z'):
+        print(ch,"is a lowercase alphabet")
+elif(ch>='0' and ch<='9'):
+        print(ch,"is a digit")
+else:
+        print(ch,"is a special character")
+    
+        
+    
+    
+   
+    
